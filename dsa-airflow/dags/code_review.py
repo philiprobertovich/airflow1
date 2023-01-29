a805bf84a94c597025dad7024555d48808ef1c79
Phil Kendall <philiprobertovich@gmail.com>
2023-01-28 23:00:21 -0800

    add list of apples and create default args.

--- dsa-airflow/dags/code_review.py
+++ dsa-airflow/dags/code_review.py
@@ -6,4 +6,13 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
+# Constant list of apples.
+APPLES =['pink lady', 'jazz', 'orange pippin', 'granny smith', 'red delicious', 'gala', 'honeycrisp', 'mcintosh', 'fuji']
 
+# Sets the default DAG args
+default_args = {
+  'start_date': days_ago(2),
+  'schedule_interval': timedelta(days=1),
+  'retries': 1,
+  'retry_delay': timedelta(minutes=5)
+}
