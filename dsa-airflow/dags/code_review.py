1d4d6f58f8cad9018afdb3e644b8e770e676265c
Phil Kendall <philiprobertovich@gmail.com>
2023-01-28 23:13:27 -0800

    instantiate DAG and create task 1 and 2.

--- dsa-airflow/dags/code_review.py
+++ dsa-airflow/dags/code_review.py
@@ -9,6 +9,11 @@ from airflow.utils.dates import days_ago
 # Constant list of apples.
 APPLES =['pink lady', 'jazz', 'orange pippin', 'granny smith', 'red delicious', 'gala', 'honeycrisp', 'mcintosh', 'fuji']
 
+# Task that will read and print content from the txt file along with a greeting
+def print_hello():
+  with open("code_review.txt", "r") as name_file:
+    print(f"Hello {name_file}!")
+
 # Sets the default DAG args
 default_args = {
   'start_date': days_ago(2),
@@ -16,3 +21,22 @@ default_args = {
   'retries': 1,
   'retry_delay': timedelta(minutes=5)
 }
+
+# Instantiate a DAG
+with DAG(
+  'code_review',
+  description='Code review DAG that will run a series of tasks including outputting a name to a txt file and then reading it, and then picking a random name of apples from a list 3 times.',
+  default_args=default_args
+) as dag:
+
+  # Task that will write my name to txt file
+  task_1 = BashOperator(
+    task_id = 'task_1',
+    bash_command='echo "Philip" > code_review.txt'
+  )
+
+  # Task that will will execute print_hello function
+  task_2 = PythonOperator(
+    task_id="task_2",
+    python_callable=print_hello
+  )
